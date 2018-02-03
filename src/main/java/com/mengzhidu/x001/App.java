cd5043dbd74668d47ad1f658e6d74c088dbc9af5
xinguimeng <xinguimeng@meituan.com>
2018-02-03 18:30:20 +0800

    首次提交

---
+++ src/main/java/com/mengzhidu/x001/App.java
@@ -0,0 +1,21 @@
+package com.mengzhidu.x001;
+
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * 文件读写是我们最熟悉的一个操作
+ * 我们这里就使用nio的方式来对一个文件进行读操作
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        //System.out.println(System.getProperty("user.dir"));
+        String path = "src/main/resources/demo1.txt";
+        FileInputStream inputStream = new FileInputStream(path);
+        FileChannel fileChannel = inputStream.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(100);
+        fileChannel.read(buffer);
+        System.out.println("读取的内容: " + new String(buffer.array()));
+    }
+}
---
+++ src/main/java/com/mengzhidu/x002/App.java
@@ -0,0 +1,10 @@
+package com.mengzhidu.x002;
+
+/**
+ * Created by xinguimeng on 2018/2/3
+ */
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
